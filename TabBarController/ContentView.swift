7ec9e19305404fb41937436dd22a1aaf4251b79e
Edwin <edwin12sv@gmail.com>
2023-12-30 19:18:29 -0600

    TabBar en Swift

--- TabBarController/ContentView.swift
+++ TabBarController/ContentView.swift
@@ -9,13 +9,62 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            Text("Primera Pestaña")
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("Inicio")
+                }
+
+            Text("Segunda Pestaña")
+                .tabItem {
+                    Image(systemName: "heart")
+                    Text("Favoritos")
+                }
+
+            CircleImageView(imageURL: "https://images.alphacoders.com/121/1218911.jpg")
+                .tabItem {
+                    Image(systemName: "person.crop.circle")
+                    Text("Perfil")
+                }
         }
-        .padding()
+    }
+}
+
+struct CircleImageView: View {
+    var imageURL: String
+    
+    var body: some View {
+        VStack {
+            AsyncImage(url: URL(string: imageURL)) { phase in
+                switch phase {
+                case .empty:
+                    ProgressView()
+                case .success(let image):
+                    image
+                        .resizable()
+                        .scaledToFill()  // Cambiado a .scaledToFill() para ocupar el tamaño completo
+                        .clipShape(Circle())
+                        .overlay(Circle().stroke(Color.white, lineWidth: 4))
+                        .shadow(radius: 10)
+                        .frame(width: 200, height: 200)  // Ajustado el tamaño
+                case .failure:
+                    Image(systemName: "person.crop.circle.fill")
+                        .resizable()
+                        .scaledToFill()  // Cambiado a .scaledToFill()
+                        .foregroundColor(.blue)
+                        .clipShape(Circle())
+                        .overlay(Circle().stroke(Color.white, lineWidth: 4))
+                        .shadow(radius: 10)
+                        .frame(width: 200, height: 200)  // Ajustado el tamaño
+                @unknown default:
+                    EmptyView()
+                }
+            }
+            
+            Text("Mi Perfil")
+                .padding()
+        }
     }
 }
 
