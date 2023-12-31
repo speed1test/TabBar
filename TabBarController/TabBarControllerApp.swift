3385cf3eaf3ce1e017848c351c121d01105bf753
Edwin <edwin12sv@gmail.com>
2023-12-30 18:27:11 -0600

    Initial Commit

---
+++ TabBarController/TabBarControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarControllerApp.swift
+//  TabBarController
+//
+//  Created by elsalvador on 30/12/23.
+//
+
+import SwiftUI
+
+@main
+struct TabBarControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
